d518b40b8dbaa41287437822d13ce50a1b52b63c
ohemaa-baffoe <baffoeabina@gmail.com>
2025-08-23 08:18:32 -0500

    Create 2.2.2_if_else.py

---
+++ 2.2.2_if_else.py
@@ -0,0 +1,12 @@
+# When submitting your code, 
+# make sure the value of stoplight is set to "red"
+stoplight = "red"
+
+if (stoplight == "red"):
+  print(f"The stoplight is {stoplight}, you need to stop!")
+else:
+  print(f"You can go, the stoplight is not red!")
+  
+
+  
+
