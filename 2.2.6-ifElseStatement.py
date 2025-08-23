99a45663a8a7fb9fa90e580b6c69f7f6147b23f5
ohemaa-baffoe <baffoeabina@gmail.com>
2025-08-23 08:09:48 -0500

    Create 2.2.6-ifElseStatement.py
    This is my first commit

---
+++ 2.2.6-ifElseStatement.py
@@ -0,0 +1,19 @@
+# When submitting your code, 
+# make sure the value of breathes_air is set to True and 
+# the value of breathes_water is set to False
+breathes_air = True 
+breathes_water = False
+
+#Use operstors
+if (breathes_air == True and breathes_water==False):
+    print ("Mammal")
+else:
+    print("Animal is not a mammal")
+
+
+
+ #if breathes_air = True
+  #Mammal = True
+ #else:
+  #if breathes_water = False
+ #Mammal = False
