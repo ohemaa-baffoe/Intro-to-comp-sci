41c4e2f6cafad4fbd7f3bf38c613ab9f891226ac
ohemaa-baffoe <baffoeabina@gmail.com>
2025-08-23 08:13:31 -0500

    Update 2.2.6-ifElseStatement.py

--- 2.2.6-ifElseStatement.py
+++ 2.2.6-ifElseStatement.py
@@ -1,3 +1,18 @@
+'''You are writing code to help a zoo categorize animals. 
+The program has two variables breathes_air and breathes_water depending on if the animal breathes in air or water. 
+Complete the program by writing your code where it says YOUR CODE HERE. We are starting with just one case, so your code should:
+
+Check if an animal breathes_air but does not breathes_water. Print "Mammal" if breathes_air is true and breathes_water is false.
+
+If any other state of breathes_air and breathes_water exists, print "Animal is not a mammal."
+
+You can adjust the values of breathes_air and breathes_water, but when you submit, they must have the following values:
+
+breathes_air = True
+breathes_water = False '''
+
+
+
 # When submitting your code, 
 # make sure the value of breathes_air is set to True and 
 # the value of breathes_water is set to False
